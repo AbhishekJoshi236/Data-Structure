bd01e7367c84bd2c024c802b0196cbc480f030fc
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-12-17 20:08:45 +0530

    Create Insertion_deletion_in_BST_using_single_pointer.c

---
+++ BST/Insertion_deletion_in_BST_using_single_pointer.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct tree_info
+{
+    int data;
+    struct tree_info *left,*right;
+}tree;
+
+
+tree* insertnode(tree* root,int item)
+{
+    if(root==NULL)
+    {
+        tree* newnode=(tree*)malloc(sizeof(tree));
+        newnode->data=item;
+        newnode->left=NULL;
+        newnode->right=NULL;
+        root=newnode;
+    }
+    else
+    {
+        if(item < root->data)
+            root->left=insertnode(root->left,item);
+        else
+            root->right=insertnode(root->right,item);
+    }
+  
+    return root;
+} 
+
+
+void inorderdisplay(tree*root)
+{
+    if(root!=NULL)
+    {
+        inorderdisplay(root->left);
+        printf("%d ",root->data);
+        inorderdisplay(root->right);
+    }
+}
+
+
+tree* successor(tree *root)
+{
+    root=root->right;
+    while(root->left != NULL)
+    {
+        root=root->left;
+    }
+    return root;
+}
+
+
+tree * delete_node(tree *root,int key)
+{
+    tree *suc;
+
+    if(root==NULL)
+    {   return NULL;        }
+    
+    if(root->left==NULL && root->right==NULL)
+    {
+        free(root);
+        return NULL;
+    }
+  
+    if(key< root->data)
+    {
+        root->left=delete_node(root->left,key);
+    }    
+    else if(key> root->data)
+    {
+        root->right=delete_node(root->right,key);
+    }
+    else
+    {
+        suc=successor(root);
+        root->data=suc->data;
+        printf("\nNEW VALUE AT DELETED POSITION: %d",root->data);
+        root->right=delete_node(root->right,suc->data);
+    }
+
+    return root;
+}
+
+
+int main()
+{
+    int choice,item;
+    tree *root=NULL;
+    printf("\nOperations in BST: ----");
+    while(1)
+    {
+        printf("\n1.INSERT\n2.DISPLAY\n3.DELETE\n4.EXIT\nEnter Choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1: printf("Enter data: ");
+                    scanf("%d",&item);
+                    root=insertnode(root,item);
+                    break;
+        
+            case 2: printf("\nDISPLAYING.......\n");
+                    inorderdisplay(root);
+                    printf("\n");
+                    break;
+
+            case 3: printf("Enter the data of node which u want to delete:   ");
+                    scanf("%d",&item);
+                    root=delete_node(root,item);
+                    break;
+
+            case 4: exit(0);
+            default: printf("\nINVALID CHOICE\n");
+        }
+    }
+    return 0;
+}
